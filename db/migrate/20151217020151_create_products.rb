4d97d7aa6333d62052a10dfd68444b518680e0f8
sombat <lsombat@gmail.com>
2015-12-17 12:09:20 +0700

    first commit

---
+++ db/migrate/20151217020151_create_products.rb
@@ -0,0 +1,17 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.text :description
+      t.string :image
+      t.float :unitprice
+      t.integer :stock
+      t.integer :promotion,default: 0
+      t.datetime :p_start
+      t.datetime :p_end
+      t.integer :status
+
+      t.timestamps null: false
+    end
+  end
+end
